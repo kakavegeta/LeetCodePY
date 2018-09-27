ac00929dce1bc66781daf94ef5f28ee8f54819a4
vegeka <455075468@qq.com>
2018-09-27 18:47:48 +0800

    some added

--- FourSum/FourSum.py
+++ FourSum/FourSum.py
@@ -1,27 +1,4 @@
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
 class Solution:
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: ListNode
-        :type n: int
-        :rtype: ListNode
-        """
-        dummy = ListNode("")
-        dummy.next = head
-        start = head
-        length = 0
-        while start != None:
-            start = start.next
-            length += 1
-        length = length - n
-        start = dummy
-        while length>0:
-            length-=1
-            start = start.next
-        start.next = start.next.next
-        return dummy.next 
+    def fourSum(self, nums, target):
+        nums.sort()
+        left, right = 0, len(series)
