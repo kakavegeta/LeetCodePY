f5cbf26bbd4d069ae09cb264302ccc9643808175
Zhang <jzh161@ur.rochester.edu>
2019-07-23 20:00:38 -0400

    dp

--- DynamicProgramming/FillingBookcase.py
+++ DynamicProgramming/FillingBookcase.py
@@ -1,14 +1,16 @@
 class Solution:
     def minHeightShelves(self, books, shelf_width):
-        dp = [float('inf')]*len(books)
-        for i in range(len(books)):
-            w, h = books[i][0], books[i][1]
+        dp = [0]*(len(books)+1)
+
+        for i in range(1,len(books)+1):
+            w, h = books[i-1][0], books[i-1][1]
             same_shelf_height = dp[i-1] + h
-            for j in range(i-1, -1, -1):
-                w += books[j][0]
+            dp[i] = same_shelf_height
+            for j in range(i-1, 0, -1):
+                w += books[j-1][0]
                 if w > shelf_width:
                     break
-                h = max(h, books[j][1])
-                dp[i] = max(same_shelf_height, dp[j-1]+h)
+                h = max(h, books[j-1][1])
+                dp[i] = min(dp[i], dp[j-1]+h)
         return dp[len(books)]
                 
